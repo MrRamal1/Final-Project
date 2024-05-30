f024a52414337c1f8e6249d0bcbaccf19d29742e
MrRamal1 <154679600+MrRamal1@users.noreply.github.com>
2024-05-31 00:01:42 +0400

    I created new section and initalazed swiper to my js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,14 @@
-const swiper = new Swiper('.swiper-container', {
-    direction: 'vertical',
-    loop: true,
-  
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  speed:1000,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
-  
